1567bcc206bf90ba66dca1b9b1b47916f20fea02
Ktai <krataias65@gmail.com>
2021-08-12 19:12:31 -0600

    beautify

--- tvmaze.js
+++ tvmaze.js
@@ -35,13 +35,13 @@ function populateShows(shows) {
   $showsList.empty();
 
   for (let show of shows) {
-    let showImage = '';
-    if(show.show.image !== null){
+    let showImage = "";
+    if (show.show.image !== null) {
       showImage = show.show.image.original;
-    }else{
-      showImage = 'https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300'
+    } else {
+      showImage =
+        "https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300";
     }
-
 
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.show.id}">
@@ -69,7 +69,7 @@ function populateShows(shows) {
 $("#search-form").on("submit", async function handleSearch(evt) {
   evt.preventDefault();
 
-  let input = document.querySelector('input');
+  let input = document.querySelector("input");
   let query = $("#search-query").val();
   if (!query) return;
 
@@ -78,7 +78,7 @@ $("#search-form").on("submit", async function handleSearch(evt) {
   let shows = await searchShows(query);
   populateShows(shows);
 
-  input.value = '';
+  input.value = "";
 });
 
 /** Given a show ID, return list of episodes:
@@ -90,15 +90,17 @@ async function getEpisodes(id) {
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
   // TODO: return array-of-episode-info, as described in docstring above
- const response = await axios.get(`https://api.tvmaze.com/shows/${id}/episodes`)
- const allEpisodes = response.data;
- return allEpisodes;
+  const response = await axios.get(
+    `https://api.tvmaze.com/shows/${id}/episodes`
+  );
+  const allEpisodes = response.data;
+  return allEpisodes;
 }
 
 function populateEpisodes(allEps) {
   const $episodeList = $("#episodes-list");
   $episodeList.empty();
-  
+
   for (let episode of allEps) {
     let $episode = $(
       `<list class="list-group-item">${episode.name}(season ${episode.season}, number ${episode.number})</list>`
@@ -108,15 +110,12 @@ function populateEpisodes(allEps) {
   }
 }
 
-$("#shows-list").on("click", ".epButton", async function(evt) {
+$("#shows-list").on("click", ".epButton", async function (evt) {
   $("#episodes-area").show();
-  
+
   let parentOfParentEleOfEpBtn = evt.target.parentElement.parentElement;
-  let id = parentOfParentEleOfEpBtn.dataset.showId
-  
+  let id = parentOfParentEleOfEpBtn.dataset.showId;
 
   let friends = await getEpisodes(id);
   populateEpisodes(friends);
-
-
 });
